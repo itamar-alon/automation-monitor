d5248d5bface0a101530ed77ac0126976cb34143
itamar alon <itamaralon211@gmail.com>
2026-04-19 09:56:36 +0300

    add env_example and req

--- logger_setup.py
+++ logger_setup.py
@@ -3,7 +3,6 @@ import logging_loki
 import time
 
 def get_loki_logger(job_name="qa_automation"):
-    # הגדרת ה-Handler של Loki
     handler = logging_loki.LokiHandler(
         url="http://10.77.72.45:3100/loki/api/v1/push", 
         tags={"job": job_name, "env": "production"},
@@ -12,10 +11,8 @@ def get_loki_logger(job_name="qa_automation"):
 
     logger = logging.getLogger(job_name)
     
-    # מוודא שלא מוסיפים את אותו Handler פעמיים אם קוראים לפונקציה שוב
     if not logger.handlers:
         logger.addHandler(handler)
-        # מוסיף גם הדפסה לטרמינל הרגיל כדי שתראה מה קורה במקביל
         logger.addHandler(logging.StreamHandler())
         logger.setLevel(logging.INFO)
     
